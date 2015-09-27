2b7208066d43e389f310dcee6c259505d3607aa2
Zsolt Imre <imrexzsolt@gmail.com>
2015-09-27 18:53:29 +0000

    Updated gitignore not to include the C object files and session files. Updated dev_commit script

--- dev_commit.sh
+++ dev_commit.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-./engine/engine.py stop
-sleep 2
-find ./ -name "*.pyc" -type f -exec rm {} \;
-find ./engine/jobs/ -name "*.crash*" -type f -exec rm {} \;
-find ./engine/jobs/ -name "*.session" -type f -exec rm {} \;
-find ./agents/ -name "*.o" -type f -exec rm {} \;
-
 git add -A .
 git commit -m "$1"
 git push origin master
